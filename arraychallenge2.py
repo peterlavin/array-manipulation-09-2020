ae8ff895d9a0fadb8cf8578fc4dd13ecb7e0ece8
peterlavin <peterlavin01@gmail.com>
2020-09-23 21:11:33 +0100

    files renamed/moved

--- arraychallenge2.py
+++ arraychallenge2.py
@@ -1,52 +1,38 @@
-'''
-Created on 12 Sep 2020
 
-@author: peter
-'''
+from datetime import datetime as dt
+import pytest
 
-import unittest
 
-def calc_array(arr: list) -> list:
-    
-    dbl_arr = [[item,0] for item in arr]
+def array_counts(arr: []) -> []:
 
+    dbl_arr = [[itm, 0] for itm in arr]
+        
     for count, item in enumerate(dbl_arr):
         if count == 0:
             continue
-        for back_count in range(1,count+1):
-            item[1] += abs(item[0] - dbl_arr[count - back_count][0])
-            
+        for back_count in range(1, count + 1):  # up to but not including
+            if dbl_arr[count - back_count][0] > item[0]:
+                item[1] -= abs((item[0] - dbl_arr[count - back_count][0]))
+            elif dbl_arr[count - back_count][0] < item[0]:
+                item[1] += abs((item[0] - dbl_arr[count - back_count][0]))
     return [item[1] for item in dbl_arr]
 
-def ret_val(val):
-    return val*2
 
-class TestCalcArray(unittest.TestCase):
+class TestArrayChallenge():
     
-    def test1(self):
-        """ Increasing and equal only, example from test """
-        self.assertEqual(calc_array([1,2,2,3]),[0,1,1,4])
+    def test_array_known_1(self):
+        assert array_counts([1, 2, 2, 3]) == [0, 1, 1, 4]
+        
+    def test_array_known_2(self):
+        assert array_counts([2, 4, 3]) == [0, 2, 0]
+      
+    def test_long_array(self, big_array):
+        assert len(array_counts(big_array)) == 1500
 
-    def test2(self):
-        """ Increasing and decreasing """  
-        self.assertEqual(calc_array([1,2,5,3]),[0,1,7,5])
-        
-    def test3(self):
-        self.assertEqual(calc_array([1,12,5,3,5]),[0,11,11,13,13])
-        
-    def test4(self):
-        """Len of 2, decreasing"""
-        self.assertEqual(calc_array([5,3]),[0,2])
-        
-    def test5(self):
-        """One element list"""
-        self.assertEqual(calc_array([1]),[0])
-        
-    def test6(self):
-        """Zero element list"""
-        self.assertEqual(calc_array([]),[])
-
-if __name__ == '__main__':
-    unittest.main(verbosity = 2)
-
-    
+    @pytest.mark.parametrize(('a, expected'),
+                            [([], []),
+                            ([1], [0]),
+                            ([1, 2], [0, 1])])
+    def test_array_empty(self, a, expected):
+        assert array_counts(a) == expected     
+   
